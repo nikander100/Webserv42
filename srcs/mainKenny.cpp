92349f2d03dc3ab6483de1918f6a52f8ca87a834
Nikander van der Schee <nikander@student.codam.nl>
2024-04-19 21:54:42 +0200

    cleanup

--- srcs/mainKenny.cpp
+++ srcs/mainKenny.cpp
@@ -1,16 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: code <code@student.42.fr>                  +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/12 14:40:37 by code              #+#    #+#             */
-/*   Updated: 2024/04/12 20:19:13 by code             ###   ########.fr       */
+/*                                                        ::::::::            */
+/*   mainKenny.cpp                                      :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: code <code@student.42.fr>                    +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2024/04/12 14:40:37 by code          #+#    #+#                 */
+/*   Updated: 2024/04/19 21:54:22 by nvan-der      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../include/webServ.hpp"
+#include "Webserv.hpp"
 #include <vector>
 #include <map>
 
@@ -23,7 +23,7 @@ int	main(int argc, char **argv)
 	std::vector<std::string> bob = {"banaan", "appel", "anus"};
 	std::cout << bob.size();		
 
-	std::map<int std::string> nietbob 
+	std::map<int std::string> nietbob
 
 	if (argc > 2)
 		return (0);
