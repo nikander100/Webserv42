4950931e16129dcfc180de5a6b4cd592e828ed00
Nikander van der Schee <nikander@student.codam.nl>
2024-10-01 20:57:16 +0200

    cgi files

---
+++ get_cookie.py
@@ -0,0 +1,34 @@
+import os
+from http import cookies
+import cgi, cgitb
+import time
+
+# Add a delay to test cgi execution tomeout
+# time.sleep(15)
+
+cgitb.enable()
+
+# Create instance of FieldStorage
+form = cgi.FieldStorage()
+
+# Get data from fields
+key = form.getvalue('key')
+
+# Create a cookie
+cookie = cookies.SimpleCookie()
+if 'HTTP_COOKIE' in os.environ:
+    cookie.load(os.environ["HTTP_COOKIE"])
+
+# Prepare the response body
+if key and key in cookie:
+    response_body = f"The Value of Cookie '{key}' is {cookie[key].value}"
+else:
+    response_body = "Cookie was not found!"
+
+# Print headers
+print("HTTP/1.1 200 OK", end='\r\n')
+print("Content-Type: text/plain", end='\r\n')
+print("\r\n", end='')  # This prints the blank line separating headers from the body
+
+# Print body
+print(response_body, end='')
